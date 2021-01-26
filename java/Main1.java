d7df07252420cd8cb8451abada8650138053befc
Mark Russel Miranda <sablay296@gmail.com>
2021-01-26 20:33:48 +0000

    things I learn from amigoscode

---
+++ java/Main1.java
@@ -0,0 +1,12 @@
+public class Main1 {
+
+  public static void main(String[] args) {
+  /*
+      This is  a multiline comment
+      This prints to the console
+  */
+  System.out.println("Hooray my first JAVA by myself"):
+  
+  
+  }
+}
