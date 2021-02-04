b385da3105beeaada3e8354bdee147ec14ef1753
Daddy Awesome <sablay296@gmail.com>
2021-02-04 15:54:28 -0500

    add main input

--- learning-java/src/tutorial/MainInput.java
+++ learning-java/src/tutorial/MainInput.java
@@ -6,11 +6,18 @@ import java.util.Scanner;
 public class MainInput {
     public static void main(String[] args){
         Scanner myObj = new Scanner(System.in);
-
         System.out.println("Please enter your name:");
 
+        //String Scan
         String UrName = myObj.nextLine();
         System.out.printf("Your name is %s%n", UrName);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Give me an integer");
+
+        //Int Scan
+        int scanned =sc.nextInt();
+        System.out.println(scanned);
 
     }
 }
