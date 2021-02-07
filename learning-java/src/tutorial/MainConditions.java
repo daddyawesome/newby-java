b0e4bb4f997fb3b08e14bf083225b6165ac1f454
Daddy Awesome <sablay296@gmail.com>
2021-02-07 14:31:49 -0500

    conditions in JAVA

---
+++ learning-java/src/tutorial/MainConditions.java
@@ -0,0 +1,32 @@
+package tutorial;
+
+
+import java.util.Scanner;
+
+public class MainConditions {
+    public static void main(String[] args){
+        //TODO
+
+        int x = 64;
+        int y = 78;
+        int z = 10;
+
+        // > < == >= <= !=
+
+        boolean compare = x < y;
+
+        System.out.println(compare);
+
+        //Comparing String
+        String sx = "Hello";
+        String sy = "Hi";
+
+        boolean scompare = s.equal(sx);
+        System.out.println(scompare);
+
+        // boolean operator &, || and !()
+        boolean ocompare = !(x>y & x>z);
+        System.out.println(ocompare);
+
+    }
+}
