61f670a8fc3c1c237a787d1886d67f8d98671db5
Daddy Awesome <sablay296@gmail.com>
2021-02-07 14:48:20 -0500

    elseif in JAVA

--- learning-java/src/tutorial/MainIfElse.java
+++ learning-java/src/tutorial/MainIfElse.java
@@ -7,11 +7,21 @@ public class MainIfElse {
     public static void main(String[] args){
         //TODO
 
+        System.out.println("Please type anything:");
+
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
 
+
+
         if (s.equals("Mark")){
             System.out.println("You type Mark");
+        }else if(s.equals("Hello")){
+            System.out.println("Hi!");
+        }else if(s.equals("Hi")){
+            System.out.println("Hello!");
+        }else {
+            System.out.println("You typed something");
         }
 
     }
