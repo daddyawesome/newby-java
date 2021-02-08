7d1968299751076e99c192cd18bf6536a8123b12
Daddy Awesome <sablay296@gmail.com>
2021-02-08 11:59:47 -0500

    arrays in JAVA

---
+++ learning-java/src/tutorial/MainArray.java
@@ -0,0 +1,22 @@
+package tutorial;
+
+
+import java.util.Scanner;
+
+public class MainArray {
+    public static void main(String[] args){
+        //TODO
+
+        String[] newArr = new String[5];
+        int[] intArr = {8, 7, 6,-5};
+
+        newArr[0] = "Daddy";
+        newArr[1] = "Awesome";
+        newArr[2] = "Mark";
+        newArr[3] = "Russel";
+        newArr[4] = "name";
+
+        System.out.println(intArr[2]);
+        System.out.println(newArr[2]);
+    }
+}
